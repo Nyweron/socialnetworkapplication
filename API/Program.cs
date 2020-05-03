b2676b6637a5b4b67b02ded602e9a0c05504b66a
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-05-03 14:26:02 +0200

    init proj

---
+++ API/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = CreateHostBuilder(args).Build();
+
+      /* START  Create DB if not exist, always check when we start app*/
+      using(var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
+        try
+        {
+          var context = services.GetRequiredService<DataContext>();
+          context.Database.Migrate();
+        }
+        catch (Exception ex)
+        {
+          var logger = services.GetRequiredService<ILogger<Program>>();
+          logger.LogError(ex, "An error occured during migration");
+        }
+      }
+      /* END  Create DB if not exist, always check when we start app*/
+
+      host.Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+      Host.CreateDefaultBuilder(args)
+      .ConfigureWebHostDefaults(webBuilder =>
+      {
+        webBuilder.UseStartup<Startup>();
+      });
+  }
+}
