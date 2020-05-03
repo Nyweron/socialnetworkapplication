5008e1c7d1add623db6f6fd2e52685be6bcbd1d5
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-05-03 21:28:04 +0200

    Added CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,15 @@ namespace API
       {
         opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
       });
+
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        });
+      });
+
       services.AddControllers();
     }
 
@@ -43,6 +52,8 @@ namespace API
       }
 
       // app.UseHttpsRedirection();
+
+      app.UseCors("CorsPolicy");
 
       app.UseRouting();
 
