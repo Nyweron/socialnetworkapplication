c3a6bd1d9fffc3152f0663f0db7f252686cb4f7c
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-05-10 22:26:02 +0200

    Fixed bug with delete handler

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -23,7 +23,7 @@ namespace Application.Activities
 
       public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
       {
-        var activity = _context.Activities.FindAsync(request.Id);
+        var activity = await _context.Activities.FindAsync(request.Id);
 
         if (activity == null) throw new Exception("Could not find activity");
 
