29bb8f6c1adf93d2d74dbd442dae0f7d8b39861b
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-05-11 14:38:56 +0200

    Added frontend list and navbar

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
+
 import 'semantic-ui-css/semantic.min.css'
+import './app/layout/style.css';
 
 ReactDOM.render(
   <React.StrictMode>
