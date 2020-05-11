6b67b06d64bdb6461fc59c9010645134ade3c558
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-05-11 15:08:13 +0200

    Added details and form components into ActivityDashboard

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
 import ActivityList from './ActivityList'
+import ActivityDetails from '../details/ActivityDetails'
+import ActivityForm from '../form/ActivityForm'
 
 interface IProps {
   activities: IActivity[]
@@ -12,6 +14,10 @@ const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
     <Grid>
       <Grid.Column width={10}>
         <ActivityList activities={activities} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetails />
+        <ActivityForm />
       </Grid.Column>
     </Grid>
   )
