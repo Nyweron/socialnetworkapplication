a72b78268cbdec7acea10eb42d9fad2a2523067e
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-05-13 15:45:25 +0200

    Added an edit mode to display the form

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,36 +6,48 @@ import { NavBar } from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 interface IState {
-  activities: IActivity[]
+  activities: IActivity[];
 }
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    axios.get<IActivity[]>("http://localhost:5000/api/activities").then((response) => {
-      setActivities(response.data)
-    });
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
   }, []);
 
   const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.filter(a => a.id === id)[0])
-  }
+    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
+  };
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  };
 
   return (
     <>
-      <NavBar />
-      <Container style={{ marginTop: '7em' }}>
+      <NavBar openCreateForm={handleOpenCreateForm} />
+      <Container style={{ marginTop: "7em" }}>
         <ActivityDashboard
           activities={activities}
           selectActivity={handleSelectActivity}
           selectedActivity={selectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
         />
       </Container>
     </>
   );
-
-}
+};
 
 export default App;
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,28 +1,44 @@
-import React from 'react'
-import { Grid } from 'semantic-ui-react'
-import { IActivity } from '../../../app/models/activity'
-import ActivityList from './ActivityList'
-import ActivityDetails from '../details/ActivityDetails'
-import ActivityForm from '../form/ActivityForm'
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 interface IProps {
   activities: IActivity[];
   selectActivity: (id: string) => void;
+  setSelectedActivity: (activity: IActivity | null) => void;
   selectedActivity: IActivity | null;
+  editMode: boolean;
+  setEditMode: (id: boolean) => void;
 }
 
-const ActivityDashboard: React.FC<IProps> = ({ activities, selectActivity, selectedActivity }) => {
+const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  setSelectedActivity,
+  selectedActivity,
+  editMode,
+  setEditMode,
+}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
         <ActivityList activities={activities} selectActivity={selectActivity} />
       </Grid.Column>
       <Grid.Column width={6}>
-        {selectedActivity && <ActivityDetails activity={selectedActivity} />}
-        <ActivityForm />
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+            setSelectedActivity={setSelectedActivity}
+          />
+        )}
+        {editMode && <ActivityForm setEditMode={setEditMode} />}
       </Grid.Column>
     </Grid>
-  )
-}
+  );
+};
 
 export default ActivityDashboard;
