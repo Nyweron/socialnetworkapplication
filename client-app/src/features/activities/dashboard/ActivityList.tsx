980e6ffc71530f4bbaef7f1614d2910b39386900
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-05-23 22:50:16 +0200

    Grouping activities by date

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { Item, Segment } from "semantic-ui-react";
+import React, { useContext, Fragment } from "react";
+import { Item, Segment, Label } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import ActivityStore from "../../../app/stores/activityStore";
 import ActivityListItem from "./ActivityListItem";
@@ -9,13 +9,22 @@ const ActivityList: React.FC = () => {
   const { activitiesByDate } = activityStore;
 
   return (
-    <Segment clearing>
-      <Item.Group divided>
-        {activitiesByDate.map((activity) => (
-          <ActivityListItem key={activity.id} activity={activity} />
-        ))}
-      </Item.Group>
-    </Segment>
+    <Fragment>
+      {activitiesByDate.map(([group, activities]) => (
+        <Fragment key={group}>
+          <Label size="large" color="blue">
+            {group}
+          </Label>
+          <Segment clearing>
+            <Item.Group divided>
+              {activities.map((activity) => (
+                <ActivityListItem key={activity.id} activity={activity} />
+              ))}
+            </Item.Group>
+          </Segment>
+        </Fragment>
+      ))}
+    </Fragment>
   );
 };
 
