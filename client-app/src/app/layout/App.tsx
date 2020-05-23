7d39b6c1d9b773ad3298a587a1915b518f7970bd
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-05-23 22:17:09 +0200

    Add some changes... Try fix bug

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useContext } from "react";
+import React, { Fragment } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadingComponent";
-import ActivityStore from "../stores/activityStore";
 import { observer } from "mobx-react-lite";
 import { Route, withRouter, RouteComponentProps } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
@@ -11,25 +9,15 @@ import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
-  const activityStore = useContext(ActivityStore);
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial)
-    return <LoadingComponent content="Loading activities..." />;
-
   return (
-    <>
+    <Fragment>
       <Route exact path="/" component={HomePage} />
       <Route
         path={"/(.+)"}
         render={() => (
-          <>
+          <Fragment>
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
-              <Route exact path="/" component={HomePage} />
               <Route exact path="/activities" component={ActivityDashboard} />
               <Route path="/activities/:id" component={ActivityDetails} />
               <Route
@@ -38,10 +26,10 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
                 component={ActivityForm}
               />
             </Container>
-          </>
+          </Fragment>
         )}
       />
-    </>
+    </Fragment>
   );
 };
 
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -4,7 +4,7 @@ import { IActivity } from "../../../app/models/activity";
 import { v4 as uuid } from "uuid";
 import ActivityStore from "../../../app/stores/activityStore";
 import { observer } from "mobx-react-lite";
-import { RouteComponentProps } from "react-router-dom";
+import { RouteComponentProps } from "react-router";
 
 interface DetailParams {
   id: string;
@@ -19,7 +19,6 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
     createActivity,
     editActivity,
     submitting,
-    cancelFormOpen,
     activity: initialFormState,
     loadActivity,
     clearActivity,
@@ -37,18 +36,18 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
 
   useEffect(() => {
     if (match.params.id && activity.id.length === 0) {
-      loadActivity(match.params.id).then(() => {
-        initialFormState && setActivity(initialFormState);
-      });
+      loadActivity(match.params.id).then(
+        () => initialFormState && setActivity(initialFormState)
+      );
     }
     return () => {
       clearActivity();
     };
   }, [
-    initialFormState,
     loadActivity,
     clearActivity,
     match.params.id,
+    initialFormState,
     activity.id.length,
   ]);
 
@@ -72,52 +71,49 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
     event: FormEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = event.currentTarget;
-    setActivity({
-      ...activity,
-      [name]: value,
-    });
+    setActivity({ ...activity, [name]: value });
   };
 
   return (
     <Segment clearing>
       <Form onSubmit={handleSubmit}>
         <Form.Input
+          onChange={handleInputChange}
           name="title"
           placeholder="Title"
           value={activity.title}
-          onChange={handleInputChange}
         />
         <Form.TextArea
+          onChange={handleInputChange}
           name="description"
           rows={2}
           placeholder="Description"
           value={activity.description}
-          onChange={handleInputChange}
         />
         <Form.Input
+          onChange={handleInputChange}
           name="category"
           placeholder="Category"
           value={activity.category}
-          onChange={handleInputChange}
         />
         <Form.Input
+          onChange={handleInputChange}
           name="date"
           type="datetime-local"
           placeholder="Date"
           value={activity.date}
-          onChange={handleInputChange}
         />
         <Form.Input
+          onChange={handleInputChange}
           name="city"
           placeholder="City"
           value={activity.city}
-          onChange={handleInputChange}
         />
         <Form.Input
+          onChange={handleInputChange}
           name="venue"
           placeholder="Venue"
           value={activity.venue}
-          onChange={handleInputChange}
         />
         <Button
           loading={submitting}
@@ -127,7 +123,7 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
           content="Submit"
         />
         <Button
-          onClick={cancelFormOpen}
+          onClick={() => history.push("/activities")}
           floated="right"
           type="button"
           content="Cancel"
