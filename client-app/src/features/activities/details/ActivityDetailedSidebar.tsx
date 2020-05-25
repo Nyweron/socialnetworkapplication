c102aa59cc19d2f30e74a0db6e58a206539f2bd2
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-05-25 20:09:22 +0200

    Creating the Activity Details page

---
+++ client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ActivityDetailedSidebar = () => {
+  return (
+    <div>
+      <h1>SideBar</h1>
+    </div>
+  );
+};
+
+export default ActivityDetailedSidebar;
