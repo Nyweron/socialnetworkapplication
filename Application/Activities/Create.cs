fad1bac19b26de66ff8842ab2028f48f5c3f5520
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-05-27 21:50:52 +0200

    Adding validation in the API using Fluent Validation

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -13,14 +14,25 @@ namespace Application.Activities
     public class Command : IRequest
     {
       public Guid Id { get; set; }
-
-      [Required]
       public string Title { get; set; }
       public string Description { get; set; }
       public string Category { get; set; }
       public DateTime Date { get; set; }
       public string City { get; set; }
       public string Venue { get; set; }
+    }
+
+    public class CommandValidator : AbstractValidator<Command>
+    {
+      public CommandValidator()
+      {
+        RuleFor(x => x.Title).NotEmpty();
+        RuleFor(x => x.Description).NotEmpty();
+        RuleFor(x => x.Category).NotEmpty();
+        RuleFor(x => x.Date).NotEmpty();
+        RuleFor(x => x.City).NotEmpty();
+        RuleFor(x => x.Venue).NotEmpty();
+      }
     }
 
     public class Handler : IRequestHandler<Command>
