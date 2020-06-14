d7f3fb1ab1cc7be777c6f2dafb514fcaf47b4907
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-06-14 20:54:12 +0200

    Creating the Typescript interfaces and Axios methods

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { IActivity } from "../models/activity";
 import { history } from "../..";
 import { toast } from "react-toastify";
+import { IUser, IUserFormValues } from "../models/user";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
@@ -50,6 +51,14 @@ const Activities = {
   update: (activity: IActivity) =>
     requests.put(`/activities/${activity.id}`, activity),
   delete: (id: string) => requests.del(`/activities/${id}`),
+};
+
+const User = {
+  current: (): Promise<IUser> => requests.get("/user"),
+  login: (user: IUserFormValues): Promise<IUserFormValues> =>
+    requests.post(`/user/login`, user),
+  register: (user: IUserFormValues): Promise<IUser> =>
+    requests.post(`/user/register`, user),
 };
 
 export default {
---
+++ client-app/src/app/models/user.ts
@@ -0,0 +1,15 @@
+import { intercept } from "mobx";
+
+export interface IUser {
+  username: string;
+  displayName: string;
+  token: string;
+  image?: string;
+}
+
+export interface IUserFormValues {
+  email: string;
+  password: string;
+  displayName?: string;
+  username?: string;
+}
