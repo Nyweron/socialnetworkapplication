560b96b64627a7a541f1a5bcc2b41acc444a2089
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-07-04 21:52:16 +0200

    Adding the Mapping Profile configuration for User images

--- Application/Activities/MappingProfile.cs
+++ Application/Activities/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using Domain;
 
@@ -10,7 +11,8 @@ namespace Application.Activities
       CreateMap<Activity, ActivityDto>();
       CreateMap<UserActivity, AttendeeDto>()
         .ForMember(destination => destination.Username, options => options.MapFrom(source => source.AppUser.UserName))
-        .ForMember(destination => destination.DisplayName, options => options.MapFrom(source => source.AppUser.DisplayName));
+        .ForMember(destination => destination.DisplayName, options => options.MapFrom(source => source.AppUser.DisplayName))
+        .ForMember(destination => destination.Image, options => options.MapFrom(source => source.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
     }
   }
 }
