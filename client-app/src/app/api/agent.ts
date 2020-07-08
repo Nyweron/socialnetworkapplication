77864045a7dabcd1c1a9d7b72d8be1c0a008a3d7
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-07-08 20:49:06 +0200

    Added edit bio and displayname

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -90,6 +90,8 @@ const Profiles = {
     requests.postForm(`/photos`, photo),
   setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
   deletePhoto: (id: string) => requests.del(`/photos/${id}`),
+  updateProfile: (profile: Partial<IProfile>) =>
+    requests.put(`/profiles`, profile),
 };
 
 export default {
