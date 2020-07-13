10e826769e7ff3514ac25f523d3f8e018d6a52f5
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-07-13 21:56:37 +0200

    Fixed error
    Error: Cannot send data if the connection is not in the 'Connected' State

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -39,7 +39,9 @@ export default class ActivityStore {
       .then(() => console.log("STATECON:", this.hubConnection!.state))
       .then(() => {
         console.log("Attempting to join group");
-        this.hubConnection!.invoke("AddToGroup", activityId);
+        if (this.hubConnection!.state === "Connected") {
+          this.hubConnection!.invoke("AddToGroup", activityId);
+        }
       })
       .catch((error) => console.log("Error establishing connection: ", error));
 
