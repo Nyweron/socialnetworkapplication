5fdbb30d570f2e72f2c1410cbf711645b5b81995
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-08-08 11:22:19 +0200

    Trying run app in production mode...

--- Persistence/Migrations/20200503114447_InitialCreate.cs
+++ Persistence/Migrations/20200503114447_InitialCreate.cs
@@ -1,29 +1,35 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Persistence.Migrations
 {
-    public partial class InitialCreate : Migration
+  public partial class InitialCreate : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
+      migrationBuilder.CreateTable(
+        name: "Values",
+        columns : table => new
         {
-            migrationBuilder.CreateTable(
-                name: "Values",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    Name = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Values", x => x.Id);
-                });
-        }
+          Id = table.Column<int>(nullable: false)
+            .Annotation("MySql:ValueGenerationStrategy",
+              MySqlValueGenerationStrategy.IdentityColumn)
+            .Annotation("SqlServer:ValueGenerationStrategy",
+              SqlServerValueGenerationStrategy.IdentityColumn)
+            .Annotation("Sqlite:Autoincrement", true),
 
-        protected override void Down(MigrationBuilder migrationBuilder)
+            Name = table.Column<string>(nullable: true)
+        },
+        constraints : table =>
         {
-            migrationBuilder.DropTable(
-                name: "Values");
-        }
+          table.PrimaryKey("PK_Values", x => x.Id);
+        });
     }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable(
+        name: "Values");
+    }
+  }
 }
